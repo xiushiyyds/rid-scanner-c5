19db66d6eb21c0b3f7b7ed206ee9a050f35c1bd5
xiushiyyds <782159783@qq.com>
2026-08-14 00:05:11 +0800

    fix: SPI 40MHz->20MHz (ST7789 reliable max ~27MHz)

--- components/lcd_display/include/lcd_display.h
+++ components/lcd_display/include/lcd_display.h
@@ -61,7 +61,7 @@ extern "C" {
  * ================================================================ */
 #define LCD_WIDTH            170
 #define LCD_HEIGHT           320
-#define LCD_SPI_FREQ_HZ     (40 * 1000 * 1000)  // 40MHz SPI
+#define LCD_SPI_FREQ_HZ     (20 * 1000 * 1000)  // 20MHz SPI (ST7789 max reliable ~27MHz)
 
 /* 帧缓冲大小：170×320×2 = 108,800 bytes ≈ 106KB */
 #define LCD_FB_SIZE         (LCD_WIDTH * LCD_HEIGHT * 2)
